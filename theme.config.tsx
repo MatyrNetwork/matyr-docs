dcb51dbafed32e8ddf5a8953b842f3e3b3626d20
Achmad zailani <growtopiagame875236@gmail.com>
2024-05-23 01:04:23 +0800

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,7 @@
+
 import React from 'react'
 import { useRouter } from 'next/router'
-import type { DocsThemeConfig } from 'nextra-theme-docs'
-import { useConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
