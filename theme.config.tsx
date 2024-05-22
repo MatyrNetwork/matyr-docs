25b9c902b38bec25e7cdd7a0312ecd4727787150
Achmad zailani <growtopiagame875236@gmail.com>
2024-05-23 01:41:01 +0800

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,23 +14,23 @@ const config: DocsThemeConfig = {
     defaultMenuCollapseLevel: 10,
     toggleButton: true,
   },
-  // head: function useHead() {
-  //   return (
-  //     <>
-  //       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  //       <meta httpEquiv="Content-Language" content="en" />
-  //       <link rel="icon" href="/../static/icon.png" type="image/png" />
-  //     </>
-  //   )
-  // },
-  useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
-      return {
-        titleTemplate: '%s – MatyrNetwork'
-      }
-    }
-  }
+  head: function useHead() {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <link rel="icon" href="/../static/icon.png" type="image/png" />
+      </>
+    )
+  },
+  // useNextSeoProps() {
+  //   const { asPath } = useRouter()
+  //   if (asPath !== '/') {
+  //     return {
+  //       titleTemplate: '%s – MatyrNetwork'
+  //     }
+  //   }
+  // }
 }
 
 export default config
