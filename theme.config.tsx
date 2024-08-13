e836b154af2f0d605d1b73b626856e0ad8bda5d8
Achmad zailani <growtopiagame875236@gmail.com>
2024-08-14 06:51:16 +0800

    Updating|theme,fixing-code|Azda

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
+
 const config: DocsThemeConfig = {
     logo: (
         <span style={{ display: 'flex', alignItems: 'center' }}>
